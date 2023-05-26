e6c47e6762208cc87eebe97fd46d579444a43845
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-05-26 12:13:37 -0300

    reduce termiando

---
+++ 18-reduce.py
@@ -0,0 +1,8 @@
+import functools #para usar reduce tenemos que importar functools
+
+numbers = [1,2,3,4]
+
+result = functools.reduce(lambda counter, item: counter + item, numbers )
+
+print(result)
+
